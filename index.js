a7006400dd97e32ac7fb8f79425f0ee2ba30925f
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-06-10 18:04:00 -0500

    commit tree 78d35be5e40761f221045e941a5364aaf39702fd

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers){
+    const newArry = [arrayOfDrivers[0], arrayOfDrivers[1]]
+    return newArry
+}
+
+
+const returnLastTwoDrivers = (arrayOfDrivers1)=> {
+    const newArry1 = [arrayOfDrivers1[2],arrayOfDrivers1[3]]
+    return newArry1
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+
+
+
+function createFareMultiplier(fare){
+    const number= function(num){
+        return num*fare
+    }
+    return number
+}
+
+const fareDoubler = function(number){
+    return number*2
+}
+
+const fareTripler = function(number){
+    return number*3
+}
+
+function selectDifferentDrivers(drivers,driversName ){
+    return driversName(drivers)
+}
